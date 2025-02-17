b0862e9f3755d868e614d01baf1e4938d1d80d9b
Nəzər abdullayev <160178320+NezerAbdullayev@users.noreply.github.com>
2025-02-17 09:28:03 +0400

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Footer />
+    </div>
+  );
+}
---
+++ src/App.v2.tsx
@@ -0,0 +1,5 @@
+import "./styles.css";
+
+export default function AppV2() {
+  return <div className="App">jhj</div>;
+}
